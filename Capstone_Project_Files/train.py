a2291414bd30cd230c07679221a65cb430eaa326
Mufumi Rikhotso <36229418+Mufumi@users.noreply.github.com>
2021-09-16 14:06:13 +0200

    Update train.py

--- Capstone_Project_Files/train.py
+++ Capstone_Project_Files/train.py
@@ -20,8 +20,6 @@ track_df = track_df.to_pandas_dataframe()
 x=track_df.iloc[:,:-1]
 y=track_df.iloc[:,-1:]
 
-print(y)
-
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
 run = Run.get_context()
