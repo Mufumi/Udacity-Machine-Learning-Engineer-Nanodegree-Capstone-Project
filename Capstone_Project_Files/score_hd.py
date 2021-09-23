06796254248aa230fd62210ab5249af2c64bd796
Mufumi Rikhotso <36229418+Mufumi@users.noreply.github.com>
2021-09-23 15:27:03 +0000

    Add files via upload

--- Capstone_Project_Files/score_hd.py
+++ Capstone_Project_Files/score_hd.py
@@ -1,11 +1,42 @@
 import json
-
+import numpy as np
+import os
+import onnxruntime
+from nltk import word_tokenize
+import nltk
 
 def init():
-    print("This is init")
+    nltk.download("punkt")
+    global sess
+    sess = onnxruntime.InferenceSession(
+        os.path.join(os.getenv("model"), "model.onnx")
+
+def run(request):
+    print(request)
+    text = json.loads(request)
+    qw, qc = preprocess(text["query"])
+    cw, cc = preprocess(text["context"])
+
+    # Run inference
+    test = sess.run(
+        None,
+        {"query_word": qw, "query_char": qc, "context_word": cw, "context_char": cc},
+    )
+    start = np.asscalar(test[0])
+    end = np.asscalar(test[1])
+    ans = [w for w in cw[start : end + 1].reshape(-1)]
+    print(ans)
+    return ans
 
 
-def run(data):
-    test = json.loads(data)
-    print(f"received data {test}")
-    return f"test is {test}"
+def preprocess(word):
+    tokens = word_tokenize(word)
+
+    # split into lower-case word tokens, in numpy array with shape of (seq, 1)
+    words = np.asarray([w.lower() for w in tokens]).reshape(-1, 1)
+
+    # split words into chars, in numpy array with shape of (seq, 1, 1, 16)
+    chars = [[c for c in t][:16] for t in tokens]
+    chars = [cs + [""] * (16 - len(cs)) for cs in chars]
+    chars = np.asarray(chars).reshape(-1, 1, 1, 16)
+    return words, chars
